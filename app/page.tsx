f3933e7f3bf45834b1b9a2595e333d7317259e6d
SunMaster.Dev <sunknight030@hotmail.com>
2025-07-17 08:19:55 +0700

    Fix: แกvercel6

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LoadingCard from "@/components/card/LoadingCard"
 import LandMarkContainer from "@/components/home/LandMarkContainer"
 import { Suspense } from "react" //แสดงตอนโหลด item
 
-const page = async({searchParams}:{searchParams:{search?:string,category?:string}}) => {
+const page = async({searchParams}:{searchParams:Promise<{search?:string,category?:string}>}) => {
   //Search
   const {search,category} = await searchParams
 
